1694545dffe0604dc5c455869fa54dcea78b84a0
rooneyshuman <rooneyshuman@gmail.com>
2019-08-13 00:19:29 -0700

    Created canLoad method to verify if there is a valid appt book file

--- app/src/main/java/com/cs410j/myappts/MainActivity.java
+++ app/src/main/java/com/cs410j/myappts/MainActivity.java
@@ -2,9 +2,13 @@ package com.cs410j.myappts;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import java.io.File;
+import java.io.IOException;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -12,6 +16,29 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        if (!canLoad()) {
+            // Display error - no appointment book found on device. Enter your name to create a new appointment book.
+        }
+
+    }
+
+    private boolean canLoad() {
+        File file = new File("myAppts.txt");
+        // File does not exist, create new file
+        if (!file.exists()) {
+            try {
+                file.createNewFile();
+            }
+            catch (IOException e) {
+                Log.v("Error" ,"Unable to create new file");
+            }
+        }
+
+        // Check if file is empty
+        if (file.length() != 0)
+            return true;
+
+        return false;
     }
 
     public void helpMe(View view) {
